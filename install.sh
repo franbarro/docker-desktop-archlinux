a11f239ee10f0978497a9b973fe62cf2c668dddd
Fran Barro <67473576+franbarro@users.noreply.github.com>
2023-09-04 08:47:27 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/urs/bin/zsh
+url='https://docs.docker.com/desktop/install/archlinux/'
+
+var=`curl $url | xmllint --html --xpath '//a/@href' -  2> /dev/null | grep 'zst' | head -1 `
+
+url_download=$(echo $var | sed 's/href="//g' - | sed 's/"//g' -)
+
+sudo pacman -U $url_download
